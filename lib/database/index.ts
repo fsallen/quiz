49373dae1e2a001247bd6e4647acc69cdb3654c0
fsallen <fsallen@126.com>
2024-07-27 06:24:18 +0000

    first commit

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -10,7 +10,8 @@ export const connectToDatabase = async () => {
   if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
   cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-    dbName: 'evently',
+    dbName: 'EVENTLY',
+    // dbName: 'evently',
     // dbName: 'quizonline',
     bufferCommands: false,
   })
